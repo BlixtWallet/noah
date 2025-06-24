25619d54473f3774ef782e07cd5ce332bfe23d76
Nitesh Balusu <niteshbalusu@icloud.com>
2025-06-24 03:02:28 -0400

    working version with vector icons

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./src/screens/HomeScreen";
 import SettingsScreen from "./src/screens/SettingsScreen";
 import { createNativeBottomTabNavigator } from "@bottom-tabs/react-navigation";
+import Icon from "@react-native-vector-icons/ionicons";
+import { Platform } from "react-native";
+
 const Tab = createNativeBottomTabNavigator();
 
 export default function App() {
@@ -16,14 +19,22 @@ export default function App() {
             name="Home"
             component={HomeScreen}
             options={{
-              tabBarIcon: () => ({ sfSymbol: "book" }),
+              tabBarIcon: () => {
+                return Platform.OS === "ios"
+                  ? { sfSymbol: "book" }
+                  : Icon.getImageSourceSync("home", 24)!;
+              },
             }}
           />
           <Tab.Screen
             name="Settings"
             component={SettingsScreen}
             options={{
-              tabBarIcon: () => ({ sfSymbol: "gear" }),
+              tabBarIcon: () => {
+                return Platform.OS === "ios"
+                  ? { sfSymbol: "gear" }
+                  : Icon.getImageSourceSync("settings", 24)!;
+              },
             }}
           />
         </Tab.Navigator>
