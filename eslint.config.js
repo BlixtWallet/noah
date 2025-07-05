6facd2247b143912260417b53b7dd0aaf1fb1d37
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2025-07-05 16:28:25 +0200

    Fix linting issues regarding react compiler eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const reactCompilerPlugin = require("eslint-plugin-react-compiler");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-    plugins: ["react-compiler"],
+    plugins: {
+      "react-compiler": reactCompilerPlugin,
+    },
     rules: {
       "react-compiler/react-compiler": "error",
     },
