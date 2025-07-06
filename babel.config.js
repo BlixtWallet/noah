ee9fe0e56fa3130ac39dd86f6f2a1a8515b3085f
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2025-07-07 01:51:58 +0200

    Add Metro logging for React Compiler

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    plugins: [
+      [
+        "babel-plugin-react-compiler",
+        {
+          logger: {
+            logEvent: (filename, event) => {
+              if (event.kind === "CompileSuccess") {
+                console.log("✨ React Compiler successfully compiled: ", filename);
+              }
+
+              if (event.kind === "CompileError") {
+                console.warn("⚠️ React Compiler failed to compile: " + filename);
+                console.warn(filename, JSON.stringify(event, null, 2));
+              }
+            },
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
