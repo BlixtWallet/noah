c4acc828928508d86ffff04f6bde6b527b767b9b
Nitesh Balusu <niteshbalusu@icloud.com>
2025-08-08 20:13:06 -0400

    initial commit of rust server

---
+++ server/src/main.rs
@@ -0,0 +1,35 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "server=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health_check))
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("server started listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn health_check() -> &'static str {
+    "OK"
+}
