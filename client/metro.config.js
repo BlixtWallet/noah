b98018fe9608d287c4bc85da6e57f875bb03d3a7
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2025-08-24 23:56:41 +0400

    Fix Android build on Windows
    Metro is looking for index.ts in project root for some reason
    otherwise

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,8 +1,19 @@
-const { withNativeWind } = require("nativewind/metro");
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getSentryExpoConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "..");
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+config.resolver.extraNodeModules = {
+  "~": projectRoot,
+};
+
+module.exports = config;
